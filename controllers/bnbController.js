aa221afe57c7b6fe77505aa1e4b38a069b42136b
MattiaNatella <mattianatella@gmail.com>
2025-02-19 16:31:06 +0100

    feat: show db

--- controllers/bnbController.js
+++ controllers/bnbController.js
@@ -4,17 +4,27 @@ const index = (req, res) => {
 
     const sql = 'SELECT * FROM immobili'
 
-    connection.query(sql, (err, result) => {
-        if (err) return res.status(500).json({ error: 'query al database fallita' });
-        console.log(result)
-        res.json(result)
+    connection.query(sql, (err, results) => {
+        if (err) res.status(500).json({ error: 'query al database fallita' });
+        console.log(results)
+        res.json(results)
     })
 
 }
 
 const show = (req, res) => {
     const id = req.params.id;
-    res.send(`Sono la rotta show: ${id}`)
+    const sql = 'SELECT * FROM immobili WHERE id = ?'
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) res.status(500).json({ error: 'query al database fallita' });
+        if (results.length == 0 || results[id] === null) res.status(404).json({ error: 'Immobile non trovato' });
+
+        res.json(results[0])
+
+    })
+
+
 }
 
 const store = (req, res) => {
