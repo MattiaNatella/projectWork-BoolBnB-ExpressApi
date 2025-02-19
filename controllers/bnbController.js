124d8d25c56ed0a528db2ffcf85c47c448d3b6bc
MattiaNatella <mattianatella@gmail.com>
2025-02-19 12:59:19 +0100

    feat: show route

--- controllers/bnbController.js
+++ controllers/bnbController.js
@@ -2,7 +2,13 @@ const index = (req, res) => {
     res.send('Sono la rotta index')
 }
 
+const show = (req, res) => {
+    const id = req.params.id;
+    res.send(`Sono la rotta show: ${id}`)
+}
+
 
 export default {
-    index
+    index,
+    show
 }
--- routers/router.js
+++ routers/router.js
@@ -7,7 +7,7 @@ const router = express.Router()
 router.get('/', bnbController.index)
 
 // //SHOW
-// router.get('/:id', bnbController.show)
+router.get('/:id', bnbController.show)
 
 
 export default router
