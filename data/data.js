1e2b8b3edbea668a2a504b0a28e73d25eb221391
carlosvillaar <carlosvillar004@gmail.com>
2025-02-19 16:48:52 +0100

    rimosso .env dal tracciamento di git

--- data/data.js
+++ data/data.js
@@ -1,17 +1,16 @@
-import mysql from 'mysql2'
+import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
-})
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
 
 connection.connect((err) => {
-    if (err) throw err;
-    console.log('Sei collegato al database di BoolBnB')
-})
+  if (err) throw err;
+  console.log("Sei collegato al database di BoolBnB");
+});
 
-
-export default connection
+export default connection;
